145472ed049b2a51a3a320a6786fd2d228225fa4
ihopeyouwin <ihopeyouwon@gmail.com>
2019-09-19 20:16:29 +0300

    feat: useEffect status add

--- src/components/profile/profileinfo/profilestatusHOOK.jsx
+++ src/components/profile/profileinfo/profilestatusHOOK.jsx
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import s from './profileinfo.module.css';
 
 const ProfileStatusWithHooks = (props) => {
     let [editMode, setEditMode] = useState( false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect(() =>{
+        setStatus(props.status)
+    }, [props.status]);
+
     const startEditMode = () => {
         setEditMode(true);
     };
