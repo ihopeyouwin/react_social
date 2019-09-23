2a21ff6cd92734848b4969abe916dfe859331b65
ihopeyouwin <ihopeyouwon@gmail.com>
2019-09-24 00:35:38 +0300

    refactor: auth-reducer async-await replace

--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,7 +1,7 @@
 import {authAPI} from "../api/api";
 import {stopSubmit} from "redux-form";
 
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'auth/SET_USER_DATA';
 
 let initialState = {
     userId: null,
@@ -25,29 +25,26 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
     type: SET_USER_DATA,
     payload: {userId, email, login, isAuth}
 });
-export const getAuthUserData = () => (dispatch) => {
-    return authAPI.me().then(response => {
-        if (response.data.resultCode === 0) {
-            let {id, login, email} = response.data.data;
-            dispatch(setAuthUserData(id, email, login, true));
-        }
-    });
+export const getAuthUserData = () => async (dispatch) => {
+    let response = await authAPI.me();
+    if (response.data.resultCode === 0) {
+        let {id, login, email} = response.data.data;
+        dispatch(setAuthUserData(id, email, login, true));
+    }
 };
-export const login = (email, password, rememberMe) => (dispatch) => {
-    authAPI.login(email, password, rememberMe).then(response => {
-        if (response.data.resultCode === 0) {
-            dispatch(getAuthUserData())
-        } else {
-            let message = response.data.messages.length > 0 ? response.data.messages[0] : 'unknown error';
-            dispatch(stopSubmit("login", {_error: message}));
-        }
-    });
+export const login = (email, password, rememberMe) => async (dispatch) => {
+    let response = await authAPI.login(email, password, rememberMe);
+    if (response.data.resultCode === 0) {
+        dispatch(getAuthUserData())
+    } else {
+        let message = response.data.messages.length > 0 ? response.data.messages[0] : 'unknown error';
+        dispatch(stopSubmit("login", {_error: message}));
+    }
 };
-export const logout = (email, password, rememberMe) => (dispatch) => {
-    authAPI.logout(email, password, rememberMe).then(response => {
-        if (response.data.resultCode === 0) {
-            dispatch(setAuthUserData(null, null, null, false))
-        }
-    });
+export const logout = (email, password, rememberMe) => async (dispatch) => {
+    let response = await authAPI.logout(email, password, rememberMe);
+    if (response.data.resultCode === 0) {
+        dispatch(setAuthUserData(null, null, null, false))
+    }
 };
 export default authReducer;
