5866e730862d6cc766ccebffd601eaa9184a7ffc
ihopeyouwin <ihopeyouwon@gmail.com>
2019-10-03 02:59:52 +0300

    feat: lazy for profile and dialogs, new hoc

--- src/App.js
+++ src/App.js
@@ -3,10 +3,8 @@ import './App.css';
 import Music from './components/music/music';
 import Settings from './components/settings/settings';
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
-import DialogsContainer from "./components/dialogs/dialogsContainer";
 import NavbarContainer from "./components/navbar/navbarContainer";
 import UsersContainer from "./components/users/usersContainer";
-import ProfileContainer from "./components/profile/profileContainer";
 import HeaderContainer from "./components/header/headerContainer";
 import Login from "./components/login/login";
 import {connect, Provider} from "react-redux";
@@ -14,6 +12,10 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/preloader";
 import store from "./redux/redux-store";
+import {withSuspense} from "./hoc/withsuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/dialogs/dialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/profile/profileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -21,7 +23,7 @@ class App extends Component {
     }
 
     render() {
-        if(!this.props.initialized){
+        if (!this.props.initialized) {
             return <Preloader/>
         }
 
@@ -31,8 +33,8 @@ class App extends Component {
                     <HeaderContainer/>
                     <NavbarContainer/>
                     <div className="App-content">
-                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
-                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
+                        <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
+                        <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
                         <Route path='/users' render={() => <UsersContainer/>}/>
                         <Route path='/music' render={() => <Music/>}/>
                         <Route path='/settings' render={() => <Settings/>}/>
@@ -43,6 +45,7 @@ class App extends Component {
         );
     }
 }
+
 const mapStateToProps = (state) => ({
     initialized: state.app.initialized
 });
